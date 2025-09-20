35dc4f4f84367dc83d595d5c249d753e10a040a6
Abdelrhman <agido26@gmail.com>
2025-09-20 12:28:06 +0300

    I Add 3 feature that Find Patients By ID and Update patients Info and Adding new Patiens and Test them in Presention Layer

---
+++ Business/BusinessLayer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Data_Access;
+
+namespace Business
+{
+    public class clsBusinessLayer
+    {
+
+    
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Gendor { get; set; }
+        public string Address { get; set; }
+        public string Emaill { get; set; }
+        public string Phone { get; set; }
+
+
+        enum enMode { AddNew=0, Update=1}
+       enMode Mode=enMode.AddNew;
+
+        public clsBusinessLayer() 
+        {
+        this.Id = -1;
+            this.FirstName = "";
+            this.LastName = "";
+            this.DateOfBirth=DateTime.Now.ToString();
+            this.Address = "";
+            this.Phone = "";
+            this.Emaill = "";
+            this.Gendor = "";
+
+            Mode =enMode.AddNew;
+        }
+
+
+
+        private clsBusinessLayer(string FirtsName, string LastName,
+            string Birth,string Gender,string Emaill,string Address,string Phone)
+        {
+            this.FirstName = FirtsName;
+            this.LastName = LastName;
+            this.DateOfBirth = Birth;
+            this.Address=Address;
+            this.Phone=Phone;
+            this.Emaill=Emaill;
+            this.Gendor = Gender;
+
+            Mode = enMode.Update;
+        }
+
+        private bool  _ADDNewPatient()
+        {
+
+            this.Id = clsDataAccess.AddNewPatient(this.FirstName, this.LastName, this.DateOfBirth,this.Gendor,this.Phone,this.Address,this.Emaill);
+       
+             return (Id!=-1);
+
+        }
+
+        
+        //private bool _UpdatePatient()
+        //{
+        //   ref string FirstName, LastName , DateOfBirth , Email , Address , Phone , Gendor ;
+
+        //    clsDataAccess.updatePatientByID()
+
+        //}
+
+
+        public static clsBusinessLayer Find(int ID)
+        {
+             string FirstName="";
+             string LastName="";
+             string DateOfBirth="", Email="", Address="", Phone="", Gendor="";
+           if( clsDataAccess.FindPaitentByID(ID,ref FirstName,ref LastName,ref DateOfBirth,ref Gendor,ref Phone,ref Address,ref Email))
+           { return new clsBusinessLayer(FirstName,LastName,DateOfBirth,Gendor,Email,Address,Phone); }
+
+
+           return null;
+        }
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+
+                    return _ADDNewPatient();
+
+               // case enMode.Update:
+
+            
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Data Access/clsDataAccess.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Data_Access
+{
+    public class clsDataAccess
+    {
+
+        public static int AddNewPatient(string FirstName,
+            string LastName,string DateOfBirth,string Gendor,string Phone,string Address,string Email
+            )
+
+        {
+            int ID = -1;
+            SqlConnection ConnectionString=new SqlConnection(clsConnection.Connectionstring);
+
+            string Query = @"INSERT INTO Persons (FirstName,LastName,DateOfBirth,Gendor,Phone,Address,Email)
+                             VALUES (@FirstName,@LastName,@DateOfBirth,@Gendor,@Phone,@Address,@Email);
+
+                              DECLARE @NewPatientID INT = SCOPE_IDENTITY();
+                              
+                              INSERT INTO Patients (PersonID)
+                              VALUES (@NewPatientID);
+                              
+                              SELECT SCOPE_IDENTITY() ;
+                              ";
+            SqlCommand command = new SqlCommand(Query,ConnectionString);
+
+            command.Parameters.AddWithValue("@FirstName",FirstName);
+            command.Parameters.AddWithValue("@LastName", LastName);
+            command.Parameters.AddWithValue("@DateOfBirth", DateOfBirth);
+            command.Parameters.AddWithValue("@Gendor", Gendor);
+            command.Parameters.AddWithValue("@Address", Address);
+            command.Parameters.AddWithValue("@Email", Email);
+            command.Parameters.AddWithValue("@Phone", Phone);
+
+
+            try
+            {
+                ConnectionString.Open();
+
+                object Result = command.ExecuteScalar();
+
+                if (Result != null && int.TryParse(Result.ToString(), out int NewID))
+                {
+                    ID = NewID;
+                }
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex); }
+
+            finally
+            { ConnectionString.Close(); }
+
+                return ID;
+
+             }
+
+
+        public static bool updatePatientByID(int ID,ref string FirstName, ref string LastName, ref string DateOfBirth,
+            ref string Gendor, ref string Phone, ref string Address, ref string Email)
+        {
+            bool isUpdated = false;
+            SqlConnection ConnectionString = new SqlConnection(clsConnection.Connectionstring);
+            string query = @"UPDATE Persons
+                            SET
+                            FirstName=@FirstName,LastName=@LastName,DateOfBirth=@DateOfBirth,Gendor=@Gendor,Phone=@Phone,Address=@Address,Email=@Email
+                            from Persons
+                            JOIN Patients ON Persons.PersonID=Patients.PersonID
+                            where Patients.PatientID=@PatientID;
+                            ";
+
+            SqlCommand Command = new SqlCommand(query,ConnectionString);
+            Command.Parameters.AddWithValue("@PatientID", ID);
+            Command.Parameters.AddWithValue("@FirstName", FirstName);
+            Command.Parameters.AddWithValue("@LastName", LastName);
+            Command.Parameters.AddWithValue("@DateOfBirth", DateOfBirth);
+            Command.Parameters.AddWithValue("@Gendor", Gendor);
+            Command.Parameters.AddWithValue("@Address", Address);
+            Command.Parameters.AddWithValue("@Email", Email);
+            Command.Parameters.AddWithValue("@Phone", Phone);
+
+            try 
+            {
+                ConnectionString.Open();
+                int Reader = Command.ExecuteNonQuery();
+
+                if (Reader > 0)
+                {
+                    isUpdated = true;
+
+                }
+
+
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex);
+                isUpdated = false;
+            }
+
+            finally { ConnectionString.Close(); }
+
+            return isUpdated;
+
+
+        }
+
+
+
+        public static bool FindPaitentByID(int ID, ref string FirstName, ref string LastName, ref string DateOfBirth,
+            ref string Gendor, ref string Phone, ref string Address, ref string Email)
+        {
+            bool isFind= false;
+            SqlConnection ConnectionString = new SqlConnection(clsConnection.Connectionstring);
+
+            string Query = @"Select Persons.FirstName,Persons.LastName,Persons.DateOfBirth,
+                              Persons.Gendor,Persons.Phone,Persons.Address,Persons.Email
+                              from Persons join Patients on Patients.PersonID=Persons.PersonID 
+                              where PatientID=@PatientID;                    
+                                    ";
+
+            SqlCommand Command= new SqlCommand(Query,ConnectionString);
+            Command.Parameters.AddWithValue("@PatientID", ID);
+
+            try
+            {
+                ConnectionString.Open();
+
+                SqlDataReader Reader = Command.ExecuteReader();
+
+                if (Reader.Read())
+                {
+                    FirstName =(string) Reader["FirstName"];
+                    LastName = (string)Reader["LastName"];
+
+                    if (Reader["DateOfBirth"] != DBNull.Value)
+                    {
+                        DateOfBirth = Reader["DateOfBirth"].ToString();
+                    }
+                    else DateOfBirth = "";
+
+                    if (Reader["Address"] != DBNull.Value)
+                    { Address = (string)Reader["Address"]; }
+                    else Address = "";
+
+                    if (Reader["Email"] != DBNull.Value)
+
+                    { Email = (string)Reader["Email"]; }
+                    else Email = "";
+
+                    if (Reader["Phone"] != DBNull.Value)
+
+                    {
+                        Phone = (string)Reader["Phone"];
+                    }
+                    else Phone = "";
+
+                    if (Reader["Gendor"] != DBNull.Value)
+
+                    { Gendor = (string)Reader["Gendor"]; }
+                    else Gendor = "";
+
+                    isFind = true;
+
+                }
+                Reader.Close();
+            }
+            catch (Exception ex) { Console.WriteLine(ex); isFind = false; }
+            finally { ConnectionString.Close(); }
+            return isFind;
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
